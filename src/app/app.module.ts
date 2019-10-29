47760d99b34aa790ad821f439d964469e35ddfb7
Javier Lete <javier@eskura21.com>
2019-10-29 20:59:43 +0100

    ng g c sala

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { SalaComponent } from './sala/sala.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    SalaComponent
   ],
   imports: [
     BrowserModule,
