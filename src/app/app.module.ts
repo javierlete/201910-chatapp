741a1eda6aa8c2bc6eccce80e5fb557cab55694e
Javier Lete <javier@eskura21.com>
2019-10-30 19:00:42 +0100

    SalasComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { SalaComponent } from './sala/sala.component';
 import { UsuariosComponent } from './usuarios/usuarios.component';
+import { SalasComponent } from './salas/salas.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     SalaComponent,
-    UsuariosComponent
+    UsuariosComponent,
+    SalasComponent
   ],
   imports: [
     BrowserModule,
