8728948eb32fdd7360f69421ea5f22a810fad060
Javier Lete <javier@eskura21.com>
2019-10-30 19:00:28 +0100

    UsuariosComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { SalaComponent } from './sala/sala.component';
+import { UsuariosComponent } from './usuarios/usuarios.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    SalaComponent
+    SalaComponent,
+    UsuariosComponent
   ],
   imports: [
     BrowserModule,
